937707d30b5b51f77ecfd336bcfd4a5b47e38947
SEAVPAV PECH <seavpeavpech24@gmail.com>
2025-10-03 07:32:38 +0700

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -7,7 +7,7 @@ import {
     HarmBlockThreshold,
   } from "@google/generative-ai";
   
-  const apiKey = "Your_Gemini_API";
+  const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
